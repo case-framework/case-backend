9a19ade5b4ea9ff808f63246987fb82b882a7cb9
phev8 <phev8@users.noreply.github.com>
2024-02-23 13:55:59 +0100

    feat: add GetAllUsers method to ManagementUserDBService and implement endpoints to get all users and get one user

--- pkg/db/management-user/management-users.go
+++ pkg/db/management-user/management-users.go
@@ -5,6 +5,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (dbService *ManagementUserDBService) CreateUser(
@@ -99,4 +100,40 @@ func (dbService *ManagementUserDBService) DeleteUser(
 	}
 	_, err = dbService.collectionManagementUsers(instanceID).DeleteOne(ctx, bson.M{"_id": objID})
 	return err
+}
+
+// get all management users
+func (dbService *ManagementUserDBService) GetAllUsers(
+	instanceID string,
+	returnFullObject bool,
+) ([]*ManagementUser, error) {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	filter := bson.D{}
+
+	opts := options.Find()
+	if !returnFullObject {
+		opts = options.Find().SetProjection(bson.D{
+			{Key: "_id", Value: 1},
+			{Key: "email", Value: 1},
+			{Key: "username", Value: 1},
+			{Key: "isAdmin", Value: 1},
+		})
+	}
+
+	var users []*ManagementUser
+	cursor, err := dbService.collectionManagementUsers(instanceID).Find(ctx, filter, opts)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var user ManagementUser
+		if err := cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+	return users, nil
 }
--- services/management-api/apihandlers/user-management.go
+++ services/management-api/apihandlers/user-management.go
@@ -1,9 +1,11 @@
 package apihandlers
 
 import (
+	"log/slog"
 	"net/http"
 
 	mw "github.com/case-framework/case-backend/pkg/apihelpers/middlewares"
+	jwthandling "github.com/case-framework/case-backend/pkg/jwt-handling"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,17 +13,46 @@ func (h *HttpEndpoints) AddUserManagementAPI(rg *gin.RouterGroup) {
 	umGroup := rg.Group("/user-management")
 	umGroup.Use(mw.GetAndValidateManagementUserJWT(h.tokenSignKey))
 	umGroup.Use(mw.IsInstanceIDInJWTAllowed(h.allowedInstanceIDs))
-	umGroup.Use(mw.IsAdminUser())
 	{
 		umGroup.GET("/management-users", h.getAllManagementUsers)
+
+	}
+
+	onlyAdminGroup := umGroup.Group("/")
+	onlyAdminGroup.Use(mw.IsAdminUser())
+	{
 		umGroup.GET("/management-users/:userID", h.getManagementUser)
 	}
+
 }
 
 func (h *HttpEndpoints) getAllManagementUsers(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{"error": "not implemented"})
+	token := c.MustGet("validatedToken").(*jwthandling.ManagementUserClaims)
+
+	slog.Info("getAllManagementUsers: getting all users", slog.String("instanceID", token.InstanceID), slog.String("userID", token.Subject))
+
+	users, err := h.muDBConn.GetAllUsers(token.InstanceID, token.IsAdmin)
+	if err != nil {
+		slog.Error("getAllManagementUsers: error retrieving users", slog.String("error", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting users"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"users": users})
 }
 
 func (h *HttpEndpoints) getManagementUser(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{"error": "not implemented"})
+	token := c.MustGet("validatedToken").(*jwthandling.ManagementUserClaims)
+	userID := c.Param("userID")
+
+	slog.Info("getManagementUser: getting user", slog.String("instanceID", token.InstanceID), slog.String("userID", token.Subject), slog.String("requestedUserID", userID))
+
+	user, err := h.muDBConn.GetUserByID(token.InstanceID, userID)
+	if err != nil {
+		slog.Error("getManagementUser: error retrieving user", slog.String("error", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user": user})
 }
