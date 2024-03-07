2ca07b20fbb22c27ff3eb07525fcf44db7f42c88
phev8 <phev8@users.noreply.github.com>
2024-03-07 14:13:30 +0100

    feat: implement db functions to get participants

---
+++ pkg/types/study/assignedSurvey.go
@@ -0,0 +1,15 @@
+package study
+
+const (
+	ASSIGNED_SURVEY_CATEGORY_PRIO   = "prio"
+	ASSIGNED_SURVEY_CATEGORY_NORMAL = "normal"
+	ASSIGNED_SURVEY_CATEGORY_QUICK  = "quick"
+	ASSIGNED_SURVEY_CATEGORY_UPDATE = "update"
+)
+
+type AssignedSurvey struct {
+	SurveyKey  string `bson:"surveyKey" json:"surveyKey"`
+	ValidFrom  int64  `bson:"validFrom" json:"validFrom"`
+	ValidUntil int64  `bson:"validUntil" json:"validUntil"`
+	Category   string `bson:"category" json:"category"`
+}
---
+++ pkg/types/study/participant.go
@@ -0,0 +1,29 @@
+package study
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+const (
+	PARTICIPANT_STUDY_STATUS_ACTIVE          = "active"
+	PARTICIPANT_STUDY_STATUS_TEMPORARY       = "temporary" // for participants without a registered account
+	PARTICIPANT_STUDY_STATUS_EXITED          = "exited"
+	PARTICIPANT_STUDY_STATUS_ACCOUNT_DELETED = "accountDeleted"
+)
+
+// Participant defines the datamodel for current state of the participant in a study as stored in the database
+type Participant struct {
+	ID                  primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	ParticipantID       string               `bson:"participantID" json:"participantID"` // reference to the study specific participant ID
+	CurrentStudySession string               `bson:"currentStudySession" json:"currentStudySession"`
+	EnteredAt           int64                `bson:"enteredAt" json:"enteredAt"`
+	StudyStatus         string               `bson:"studyStatus" json:"studyStatus"` // shows if participant is active in the study - possible values: "active", "temporary", "exited". Other values are possible and are handled like "exited" on the server.
+	Flags               map[string]string    `bson:"flags" json:"flags"`
+	AssignedSurveys     []AssignedSurvey     `bson:"assignedSurveys" json:"assignedSurveys"`
+	LastSubmissions     map[string]int64     `bson:"lastSubmission" json:"lastSubmission"` // surveyKey with timestamp
+	Messages            []ParticipantMessage `bson:"messages" json:"messages"`
+}
+
+type ParticipantMessage struct {
+	ID           string `bson:"id" json:"id"`
+	Type         string `bson:"type" json:"type"`
+	ScheduledFor int64  `bson:"scheduledFor" json:"scheduledFor"`
+}
