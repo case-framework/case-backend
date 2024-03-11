7eb20cda1ac87852577919f8b792e7f7953cead4
phev8 <phev8@users.noreply.github.com>
2024-03-11 12:41:24 +0100

    feat: add db method to get one file info by id

--- pkg/db/study/participantFileInfos.go
+++ pkg/db/study/participantFileInfos.go
@@ -4,11 +4,27 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	studytypes "github.com/case-framework/case-backend/pkg/types/study"
 )
 
-// count
-
 // get one by id
+func (dbService *StudyDBService) GetParticipantFileInfoByID(instanceID string, studyKey string, fileInfoID string) (participantFileInfo studytypes.FileInfo, err error) {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	_id, err := primitive.ObjectIDFromHex(fileInfoID)
+	if err != nil {
+		return participantFileInfo, err
+	}
+
+	filter := bson.M{
+		"_id": _id,
+	}
+
+	err = dbService.collectionFiles(instanceID, studyKey).FindOne(ctx, filter).Decode(&participantFileInfo)
+	return participantFileInfo, err
+}
 
 // delete one by id
 func (dbService *StudyDBService) DeleteParticipantFileInfoByID(instanceID string, studyKey string, fileInfoID string) error {
