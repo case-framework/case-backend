7b1f7d7f80bedfd55efac1142eaa1f4fa302b1b8
phev8 <phev8@users.noreply.github.com>
2024-03-29 11:20:42 +0100

    add config init

--- services/smtp-bridge/init.go
+++ services/smtp-bridge/init.go
@@ -3,12 +3,30 @@ package main
 import (
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/case-framework/case-backend/pkg/utils"
 	"github.com/gin-gonic/gin"
 )
 
-type Config struct {
+// Environment variables
+const (
+	ENV_GIN_DEBUG_MODE              = "GIN_DEBUG_MODE"
+	ENV_SMTP_BRIDGE_API_LISTEN_PORT = "SMTP_BRIDGE_API_LISTEN_PORT"
+	ENV_CORS_ALLOW_ORIGINS          = "CORS_ALLOW_ORIGINS"
+
+	ENV_LOG_TO_FILE     = "LOG_TO_FILE"
+	ENV_LOG_FILENAME    = "LOG_FILENAME"
+	ENV_LOG_MAX_SIZE    = "LOG_MAX_SIZE"
+	ENV_LOG_MAX_AGE     = "LOG_MAX_AGE"
+	ENV_LOG_MAX_BACKUPS = "LOG_MAX_BACKUPS"
+	ENV_LOG_LEVEL       = "LOG_LEVEL"
+	ENV_LOG_INCLUDE_SRC = "LOG_INCLUDE_SRC"
+
+	ENV_API_KEYS = "API_KEYS"
+)
+
+type config struct {
 	// Gin configs
 	GinDebugMode bool     `json:"gin_debug_mode"`
 	AllowOrigins []string `json:"allow_origins"`
@@ -18,7 +36,7 @@ type Config struct {
 }
 
 func init() {
-	initLogger()
+	readConfigForAndInitLogger()
 
 	conf = initConfig()
 	if !conf.GinDebugMode {
@@ -52,4 +70,22 @@ func readConfigForAndInitLogger() {
 	}
 
 	utils.InitLogger(level, includeSrc, logFilename, logFileMaxSize, logFileMaxAge, logFileMaxBackups)
+}
+
+func initConfig() config {
+	conf := config{}
+	conf.GinDebugMode = os.Getenv(ENV_GIN_DEBUG_MODE) == "true"
+	conf.Port = os.Getenv(ENV_SMTP_BRIDGE_API_LISTEN_PORT)
+	conf.AllowOrigins = strings.Split(os.Getenv(ENV_CORS_ALLOW_ORIGINS), ",")
+
+	apiKeys := os.Getenv(ENV_API_KEYS)
+	if apiKeys != "" {
+		conf.ApiKeys = strings.Split(apiKeys, ",")
+	}
+
+	if len(conf.ApiKeys) == 0 {
+		panic("No API keys provided for SMTP Bridge API. Please set the API_KEYS environment variable.")
+	}
+
+	return conf
 }
