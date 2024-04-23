9a508f5cf5f47f260109e9605d72747f8f36851e
phev8 <phev8@users.noreply.github.com>
2024-04-23 18:04:39 +0200

    add OTP db methods

--- pkg/db/participant-user/otps.go
+++ pkg/db/participant-user/otps.go
@@ -1,9 +1,13 @@
 package participantuser
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	userTypes "github.com/case-framework/case-backend/pkg/user-management/types"
 )
 
 const (
@@ -32,4 +36,46 @@ func (dbService *ParticipantUserDBService) CreateIndexForOTPs(instanceID string)
 		},
 	)
 	return err
+}
+
+func (dbService *ParticipantUserDBService) CreateOTP(instanceID string, userID string, code string, t string) error {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	otp := userTypes.OTP{
+		UserID:    userID,
+		Code:      code,
+		Type:      t,
+		CreatedAt: time.Now(),
+	}
+	_, err := dbService.collectionOTPs(instanceID).InsertOne(ctx, otp)
+	return err
+}
+
+func (dbService *ParticipantUserDBService) FindOTP(instanceID string, userID string, code string) (userTypes.OTP, error) {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	filter := bson.M{"userID": userID, "code": code}
+	var otp userTypes.OTP
+	err := dbService.collectionOTPs(instanceID).FindOne(ctx, filter).Decode(&otp)
+	return otp, err
+}
+
+func (dbService *ParticipantUserDBService) DeleteOTP(instanceID string, userID string, code string) error {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	filter := bson.M{"userID": userID, "code": code}
+	_, err := dbService.collectionOTPs(instanceID).DeleteOne(ctx, filter)
+	return err
+}
+
+func (dbService *ParticipantUserDBService) CountOTP(instanceID string, userID string) (int64, error) {
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	filter := bson.M{"userID": userID}
+	count, err := dbService.collectionOTPs(instanceID).CountDocuments(ctx, filter)
+	return count, err
 }
