14942f2fd0d2481f5edce7ae6d09461b6468d8f2
phev8 <phev8@users.noreply.github.com>
2024-04-23 15:46:13 +0200

    add middleware method to extract and validate participant JWT

--- pkg/apihelpers/middlewares/getAndValidateJWT.go
+++ pkg/apihelpers/middlewares/getAndValidateJWT.go
@@ -33,6 +33,28 @@ func GetAndValidateManagementUserJWT(tokenSignKey string) gin.HandlerFunc {
 	}
 }
 
+func GetAndValidateParticipantUserJWT(tokenSignKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := extractToken(c)
+		if err != nil {
+			slog.Warn("no Authorization token found")
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+
+		// Parse and validate token
+		parsedToken, ok, err := jwthandling.ValidateParticipantUserToken(token, tokenSignKey)
+		if err != nil || !ok {
+			slog.Warn("token validation failed")
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "error during token validation"})
+			c.Abort()
+			return
+		}
+		c.Set("validatedToken", parsedToken)
+	}
+}
+
 func extractToken(c *gin.Context) (string, error) {
 	req := c.Request
 
