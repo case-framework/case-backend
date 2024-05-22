df8c2c2dcbaf89848976802eb7061ea118c0c6e8
phev8 <phev8@users.noreply.github.com>
2024-05-22 09:56:28 +0200

    init config and modules

---
+++ jobs/study-timer/init.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/case-framework/case-backend/pkg/db"
+	"github.com/case-framework/case-backend/pkg/study"
+	"github.com/case-framework/case-backend/pkg/study/studyengine"
+	"github.com/case-framework/case-backend/pkg/utils"
+	"gopkg.in/yaml.v2"
+
+	studyDB "github.com/case-framework/case-backend/pkg/db/study"
+)
+
+// Environment variables
+const (
+	ENV_CONFIG_FILE_PATH = "CONFIG_FILE_PATH"
+
+	// Variables to override "secrets" in the config file
+	ENV_STUDY_DB_USERNAME = "STUDY_DB_USERNAME"
+	ENV_STUDY_DB_PASSWORD = "STUDY_DB_PASSWORD"
+)
+
+type config struct {
+	// Logging configs
+	Logging utils.LoggerConfig `json:"logging" yaml:"logging"`
+
+	// DB configs
+	DBConfigs struct {
+		StudyDB db.DBConfigYaml `json:"study_db" yaml:"study_db"`
+	} `json:"db_configs" yaml:"db_configs"`
+
+	InstanceIDs []string `json:"instance_ids" yaml:"instance_ids"`
+
+	// Study module config
+	StudyConfigs struct {
+		GlobalSecret string `json:"global_secret" yaml:"global_secret"`
+
+		ExternalServices []studyengine.ExternalService `json:"external_services" yaml:"external_services"`
+	} `json:"study_configs" yaml:"study_configs"`
+}
+
+var conf config
+
+var (
+	studyDBService *studyDB.StudyDBService
+)
+
+func init() {
+	// Read config from file
+	yamlFile, err := os.ReadFile(os.Getenv(ENV_CONFIG_FILE_PATH))
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.UnmarshalStrict(yamlFile, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Init logger:
+	utils.InitLogger(
+		conf.Logging.LogLevel,
+		conf.Logging.IncludeSrc,
+		conf.Logging.LogToFile,
+		conf.Logging.Filename,
+		conf.Logging.MaxSize,
+		conf.Logging.MaxAge,
+		conf.Logging.MaxBackups,
+		conf.Logging.CompressOldLogs,
+	)
+
+	// Override secrets from environment variables
+	secretsOverride()
+
+	// init db
+	initDBs()
+
+	// init study service
+	initStudyService()
+}
+
+func secretsOverride() {
+	// Override secrets from environment variables
+
+	if dbUsername := os.Getenv(ENV_STUDY_DB_USERNAME); dbUsername != "" {
+		conf.DBConfigs.StudyDB.Username = dbUsername
+	}
+
+	if dbPassword := os.Getenv(ENV_STUDY_DB_PASSWORD); dbPassword != "" {
+		conf.DBConfigs.StudyDB.Password = dbPassword
+	}
+
+}
+
+func initDBs() {
+	var err error
+	studyDBService, err = studyDB.NewStudyDBService(db.DBConfigFromYamlObj(conf.DBConfigs.StudyDB, conf.InstanceIDs))
+	if err != nil {
+		slog.Error("Error connecting to Study DB", slog.String("error", err.Error()))
+		panic(err)
+	}
+}
+
+func initStudyService() {
+	study.Init(
+		studyDBService,
+		conf.StudyConfigs.GlobalSecret,
+		conf.StudyConfigs.ExternalServices,
+	)
+}
