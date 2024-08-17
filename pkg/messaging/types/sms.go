61fb6ce4dfd15fab4b0ebb9229689c224c788c1e
phev8 <phev8@users.noreply.github.com>
2024-08-17 16:16:58 +0200

    add DB collection and methods for SMS templates

--- pkg/messaging/types/sms.go
+++ pkg/messaging/types/sms.go
@@ -1,6 +1,10 @@
 package types
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type SentSMS struct {
 	ID          string    `bson:"_id" json:"id"`
@@ -8,4 +12,12 @@ type SentSMS struct {
 	MessageType string    `bson:"messageType" json:"messageType"`
 	SentAt      time.Time `bson:"sentAt" json:"sentAt"`
 	PhoneNumber string    `bson:"phoneNumber" json:"phoneNumber"`
+}
+
+type SMSTemplate struct {
+	ID              primitive.ObjectID  `bson:"_id" json:"id,omitempty"`
+	MessageType     string              `bson:"messageType" json:"messageType"`
+	DefaultLanguage string              `bson:"defaultLanguage" json:"defaultLanguage"`
+	From            string              `bson:"from" json:"from"`
+	Translations    []LocalizedTemplate `bson:"translations" json:"translations"`
 }
