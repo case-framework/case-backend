ab4e9e8950b02afe6e104d9b50beb75a7839b6f6
Ajit Dusane <ajitdusane@gmail.com>
2024-11-13 14:29:48 +0100

    email file is saved as .eml instead of .html

--- services/smtp-bridge-emulator/apihandlers/send-email.go
+++ services/smtp-bridge-emulator/apihandlers/send-email.go
@@ -13,8 +13,6 @@ import (
 
 	messagingTypes "github.com/case-framework/case-backend/pkg/messaging/types"
 
-	"regexp"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,6 +23,10 @@ func (h *HttpEndpoints) AddRoutes(rg *gin.RouterGroup) {
 		mw.HasValidAPIKey(h.apiKeys),
 		h.sendEmail)
 }
+
+const (
+	EML_FILE_EXTENSION = ".eml"
+)
 
 type SendEmailReq struct {
 	To              []string                        `json:"to"`
@@ -43,7 +45,7 @@ func createFolder(folderPath, recipient string) error {
 	return nil
 }
 
-func saveEmailAsHtml(email SendEmailReq) error {
+func saveEmailAsEML(email SendEmailReq) error {
 	for _, recipient := range email.To {
 
 		// Create the recipient's folder and handle any error
@@ -53,58 +55,52 @@ func saveEmailAsHtml(email SendEmailReq) error {
 			return err
 		}
 
-		// Generate a unique HTML file path
-		htmlFilePath, err := getUniqueHTMLFilePath(folderPath, email.Subject)
+		// Generate a unique EML file path
+		emlFilePath, err := getUniqueEMLFilePath(folderPath)
 		if err != nil {
-			slog.Error("Error generating HTML file path", slog.String("recipient", recipient), slog.String("error", err.Error()))
+			slog.Error("Error generating EML file path", slog.String("recipient", recipient), slog.String("error", err.Error()))
 			return err
 		}
 
-		// Write the HTML content to the file
-		if err := os.WriteFile(htmlFilePath, []byte(email.Content), 0644); err != nil {
-			slog.Error("Error writing HTML file for "+recipient, slog.String("error", err.Error()))
+		// Write the EML content to the file
+		if err := os.WriteFile(emlFilePath, []byte(email.Content), 0644); err != nil {
+			slog.Error("Error writing EML file for "+recipient, slog.String("error", err.Error()))
 			return err
 		}
 
-		slog.Info("Successfully created folder '" + recipient + "' and its content as html file inside the folder")
+		slog.Info("Successfully created folder '" + recipient + "' and its content as EML file inside the folder")
 	}
 
 	return nil
 }
 
-// returns a unique file path for the HTML file, appending a counter if needed.
-func getUniqueHTMLFilePath(folderPath, subject string) (string, error) {
-	baseFileName := getHTMLFilename(subject)
-	htmlFilePath := filepath.Join(folderPath, baseFileName)
+// returns a unique file path for the EML file, appending a counter if needed.
+func getUniqueEMLFilePath(folderPath string) (string, error) {
+	baseFileName := getEMLFilename()
+	emlFilePath := filepath.Join(folderPath, baseFileName)
 	counter := 1
 
 	// Check if the file already exists, and append a counter if necessary
 	for {
-		if _, err := os.Stat(htmlFilePath); errors.Is(err, os.ErrNotExist) {
+		if _, err := os.Stat(emlFilePath); errors.Is(err, os.ErrNotExist) {
 			break
 		}
-		baseName := filepath.Base(htmlFilePath)
-		ext := filepath.Ext(htmlFilePath)
+		baseName := filepath.Base(emlFilePath)
+		ext := filepath.Ext(emlFilePath)
 		baseNameWithoutExt := baseName[:len(baseName)-len(ext)]
 
-		htmlFilePath = filepath.Join(folderPath, baseNameWithoutExt+"_"+strconv.Itoa(counter)+".html")
+		emlFilePath = filepath.Join(folderPath, baseNameWithoutExt+"_"+strconv.Itoa(counter)+EML_FILE_EXTENSION)
 		counter++
 	}
 
-	return htmlFilePath, nil
+	return emlFilePath, nil
 }
 
-// generates a valid file name for the HTML file based on the subject.
-func getHTMLFilename(filePath string) string {
-	invalidChars := regexp.MustCompile(`[\/\\:?"<>|]`)
-	sanitizedFileName := invalidChars.ReplaceAllString(filePath, "_")
-
-	if len(sanitizedFileName) > 10 {
-		sanitizedFileName = sanitizedFileName[:10]
-	}
+// generates a valid file name for the EML file
+func getEMLFilename() string {
 
 	timestamp := time.Now().Format("20060102_150405")
-	fileName := timestamp + "_" + sanitizedFileName + ".html"
+	fileName := timestamp + EML_FILE_EXTENSION
 
 	return fileName
 }
@@ -123,12 +119,12 @@ func (h *HttpEndpoints) sendEmail(c *gin.Context) {
 		return
 	}
 
-	if err := saveEmailAsHtml(req); err != nil {
-		slog.Error("Email could not be saved into HTML file(s)", slog.String("error", err.Error()))
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Email could not be saved into HTML file(s)"})
+	if err := saveEmailAsEML(req); err != nil {
+		slog.Error("Email could not be saved as EML file(s)", slog.String("error", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Email could not be saved as EML file(s)"})
 		return
 	}
 
-	slog.Info("Email has been saved into HTML file(s)")
-	c.JSON(http.StatusOK, gin.H{"message": "Email has been saved into HTML file(s)"})
+	slog.Info("Email has been saved as EML file(s)")
+	c.JSON(http.StatusOK, gin.H{"message": "Email has been saved as EML file(s)"})
 }
