227737e6eceeff8121e50bdaf36a3dd4ea622038
phev8 <phev8@users.noreply.github.com>
2024-12-20 09:22:46 +0100

    add linking codes attribute to participants

--- pkg/study/types/participant.go
+++ pkg/study/types/participant.go
@@ -17,6 +17,7 @@ type Participant struct {
 	EnteredAt           int64                `bson:"enteredAt" json:"enteredAt"`
 	StudyStatus         string               `bson:"studyStatus" json:"studyStatus"` // shows if participant is active in the study - possible values: "active", "temporary", "exited". Other values are possible and are handled like "exited" on the server.
 	Flags               map[string]string    `bson:"flags" json:"flags"`
+	LinkingCodes        map[string]string    `bson:"linkingCodes" json:"linkingCodes"`
 	AssignedSurveys     []AssignedSurvey     `bson:"assignedSurveys" json:"assignedSurveys"`
 	LastSubmissions     map[string]int64     `bson:"lastSubmission" json:"lastSubmissions"` // surveyKey with timestamp
 	Messages            []ParticipantMessage `bson:"messages" json:"messages"`
