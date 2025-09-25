1727d52df8a9d163cb5a7d9cf8830086c6ac6434
phev8 <phev8@users.noreply.github.com>
2025-09-25 10:02:17 +0200

    feat: implement comprehensive DB migration tasks and index management
    - Added functions to drop and create indexes for various databases based on configuration settings.
    - Implemented migration tasks to fix participant user contact information, enhancing data integrity.
    - Integrated structured logging for error handling during migration tasks, improving monitoring and debugging capabilities.

--- jobs/db-migration/main.go
+++ jobs/db-migration/main.go
@@ -1,7 +1,82 @@
 package main
 
-import "fmt"
+import "log/slog"
 
 func main() {
-	fmt.Println("Hello, World!")
+	dropIndexes()
+
+	createIndexes()
+
+	migrationTasks()
+}
+
+func dropIndexes() {
+	switch conf.TaskConfigs.DropIndexes.StudyDB {
+	case DropIndexesModeAll:
+		studyDBService.DropAllIndexes()
+	case DropIndexesModeDefaults:
+		studyDBService.DropDefaultIndexes()
+	}
+
+	switch conf.TaskConfigs.DropIndexes.ParticipantUserDB {
+	case DropIndexesModeAll:
+		participantUserDBService.DropIndexes(true)
+	case DropIndexesModeDefaults:
+		participantUserDBService.DropIndexes(false)
+	}
+
+	switch conf.TaskConfigs.DropIndexes.ManagementUserDB {
+	case DropIndexesModeAll:
+		managementUserDBService.DropIndexes(true)
+	case DropIndexesModeDefaults:
+		managementUserDBService.DropIndexes(false)
+	}
+
+	switch conf.TaskConfigs.DropIndexes.GlobalInfosDB {
+	case DropIndexesModeAll:
+		globalInfosDBService.DropIndexes(true)
+	case DropIndexesModeDefaults:
+		globalInfosDBService.DropIndexes(false)
+	}
+
+	switch conf.TaskConfigs.DropIndexes.MessagingDB {
+	case DropIndexesModeAll:
+		messagingDBService.DropIndexes(true)
+	case DropIndexesModeDefaults:
+		messagingDBService.DropIndexes(false)
+	}
+}
+
+func createIndexes() {
+	if conf.TaskConfigs.CreateIndexes.StudyDB {
+		studyDBService.CreateDefaultIndexes()
+	}
+
+	if conf.TaskConfigs.CreateIndexes.ParticipantUserDB {
+		participantUserDBService.CreateDefaultIndexes()
+	}
+
+	if conf.TaskConfigs.CreateIndexes.ManagementUserDB {
+		managementUserDBService.CreateDefaultIndexes()
+	}
+
+	if conf.TaskConfigs.CreateIndexes.GlobalInfosDB {
+		globalInfosDBService.CreateDefaultIndexes()
+	}
+
+	if conf.TaskConfigs.CreateIndexes.MessagingDB {
+		messagingDBService.CreateDefaultIndexes()
+	}
+}
+
+func migrationTasks() {
+	// Fix participant user contact infos
+	if conf.TaskConfigs.MigrationTasks.ParticipantUserContactInfosFix {
+		for _, instanceID := range participantUserDBService.InstanceIDs {
+			err := participantUserDBService.FixFieldNameForContactInfos(instanceID)
+			if err != nil {
+				slog.Error("Error fixing participant user contact infos", slog.String("instanceID", instanceID), slog.String("error", err.Error()))
+			}
+		}
+	}
 }
