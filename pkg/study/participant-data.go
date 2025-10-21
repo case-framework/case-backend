9f449407f59e0a43f0d92fb210a9738b2a9827b7
phev8 <phev8@users.noreply.github.com>
2025-10-21 14:19:20 +0200

    refactor: optimize findSurveyItemDef function for improved performance and readability

--- pkg/study/participant-data.go
+++ pkg/study/participant-data.go
@@ -546,11 +546,11 @@ func findSurveyItemDef(item *studyTypes.SurveyItem, key string) *studyTypes.Surv
 		return item
 	}
 
-	for _, itemDef := range item.Items {
-		if itemDef.Key == key {
-			return &itemDef
-		} else if strings.HasPrefix(key, itemDef.Key+".") {
-			return findSurveyItemDef(&itemDef, key)
+	for i := range item.Items {
+		if item.Items[i].Key == key {
+			return &item.Items[i]
+		} else if strings.HasPrefix(key, item.Items[i].Key+".") {
+			return findSurveyItemDef(&item.Items[i], key)
 		}
 	}
 	return nil
